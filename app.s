82192922bc8686e491e21ef5631abf1bf3e6fa6d
AgusMollea <102273374+AgusMollea@users.noreply.github.com>
2022-05-28 16:13:39 -0300

    Change of the principal idea
    It was decided to keep just the principal geometries and configs.

--- app.s
+++ app.s
@@ -6,178 +6,28 @@
 
 .globl main
 main:
+	bl config
+
+	bl exitProgram
+
+
+config:
+	//Guardado registro return
+	sub sp, sp, 16
+	str x30, [sp]
+
+	// Input values:
+		// the function does not receive any value
+
 	//Set saved values
  	mov x20, x0	            // Save framebuffer base address to x20
     mov x21, SCREEN_WIDTH   // Save Screen Width
     mov x22, SCREEN_HEIGHT  // Save Screen Height
 
-	bl createBackground
+	//Initialize optional variables with Zero
+	mov x3, 0x00000000
+	mov x4, 0x00000000
 
-	bl createFloor
-
-	//Example of Delay implementation
-	mov x7, 0xffffffff
-	bl delay
-
-	bl createFlag
-
-	bl createRocket
-
-	bl exitProgram
-
-
-
-createBackground:
-	//Guardado registro return
-	sub sp, sp, 16
-	str x30, [sp]
-
-	//Create Background
-	//Parameters
-	//Color:
-	movz x0, 0x0009, lsl 16 
-	movk x0, 0x0936, lsl 0
-
-	mov x1, 0 				//Coord del primero en Y
-	mov x2, 0				//Coord del primero en X
-	mov x5, 360				//Rectangle Height
-	mov x6, SCREEN_WIDTH	//Rectangle Width
-	bl createRectangle
-
-	//Carga del registro de return y devolucion del siguiente
-	ldr x30, [sp]
-	add sp, sp, 16
-	ret
-
-
-createFloor:
-	//Guardado registro return
-	sub sp, sp, 16
-	str x30, [sp]
-
-	//Create Floor
-	//Parameters:
-	//Color:
-	movz x0, 0x009d, lsl 16 
-	movk x0, 0x5a4e, lsl 0
-
-	mov x1, 360 			//Coord del primero en Y
-	mov x2, 0 				//Coord del primero en X
-	mov x5, 120				//Rectangle Height
-	mov x6, SCREEN_WIDTH	//Rectangle Width
-	bl createRectangle
-
-	//Carga del registro de return y devolucion del siguiente
-	ldr x30, [sp]
-	add sp, sp, 16
-	ret
-
-
-createFlag:
-	//Guardado registro return
-	sub sp, sp, 16
-	str x30, [sp]
-
-	//Crear Mastil de la Bandera
-	//Parametros
-	//Color
-	movz x0, 0x0000, lsl 16 
-	movk x0, 0x0000, lsl 0
-
-	mov x1, 325 	//Coord del primero en Y
-	mov x2, 420 	//Coord del primero en X
-	mov x5, 40 		//Rectangle Height
-	mov x6, 2 		//Rectangle Width
-	bl createRectangle
-
-	//Crear Azul de la Bandera
-	//Parametros
-	//Color
-	movz x0, 0x0021, lsl 16 
-	movk x0, 0x96f3, lsl 0
-
-	mov x1, 325 	//Coord del primero en Y
-	mov x2, 422 	//Coord del primero en X
-	mov x5, 15 		//Rectangle Height
-	mov x6, 18 		//Rectangle Width
-	bl createRectangle
-
-	//Crear Blanco de la Bandera
-	//Parametros
-	//Color
-	movz x0, 0x00ff, lsl 16 
-	movk x0, 0xffff, lsl 0
-
-	mov x1, 330 	//Coord del primero en Y
-	mov x2, 422 	//Coord del primero en X
-	mov x5, 5 		//Rectangle Height
-	mov x6, 18 		//Rectangle Width
-	bl createRectangle
-
-	//Carga del registro de return y devolucion del siguiente
-	ldr x30, [sp]
-	add sp, sp, 16
-	ret
-
-
-createRocket:
-	//Guardado registro return
-	sub sp, sp, 16
-	str x30, [sp]
-	
-
-	//Crear Negro de la Capsula
-	//Parametros
-	//Color
-	movz x0, 0x0000, lsl 16 
-	movk x0, 0x0000, lsl 0
-
-	mov x1, 250 	//Coord del primero en Y
-	mov x2, 300 	//Coord del primero en X
-	mov x5, 50 		//Rectangle Height
-	mov x6, 50 		//Rectangle Width
-	bl createRectangle
-
-	//Crear Azul de la Capsula
-	//Parametros
-	//Color
-	movz x0, 0x0015, lsl 16 
-	movk x0, 0x6ac0, lsl 0
-	mov x1, 252 	//Coord del primero en Y
-	mov x2, 302 	//Coord del primero en X
-	mov x5, 3 		//Rectangle Height
-	mov x6, 46 		//Rectangle Width
-	bl createRectangle
-
-	//Crear blanco de la Capsula
-	//Parametros
-	//Color
-	movz x0, 0x00ff, lsl 16 
-	movk x0, 0xffff, lsl 0
-
-	mov x1, 258 	//Coord del primero en Y
-	mov x2, 303 	//Coord del primero en X
-	mov x5, 40		//Rectangle Height
-	mov x6, 44 		//Rectangle Width
-	bl createRectangle
-
-	//Crear Negro del triangulo
-	//Parametros
-		//Color
-	movz x0, 0x0000, lsl 16 
-	movk x0, 0x0000, lsl 0
-		
-	mov x1, 243 	//Coord del primero en Y
-	mov x2, 303 	//Coord del primero en X
-	mov x5, 8		//Triangle Height
-	mov x6, 44		//Triangle Width
-
-	bl createTriangle
-
-	//Carga del registro de return y devolucion del siguiente
-	ldr x30, [sp]
-	add sp, sp, 16
-	ret
 
 createRectangle:
 	//Guardado registro return
@@ -188,14 +38,14 @@ createRectangle:
 	    // - x0:    Color Base
         // - x1:    Coord primero en y
         // - x2:    Coord prim en x
-        // - x3:    Desplazamiento en y
-        // - x4:    Desplazamiento en x
         // - x5:    Alto del rectangulo
         // - x6:    Ancho del rectangulo
 
     //Temporary values:
 		// - x9: 	Temp Base Address of Rectangle
 		// - x10:	Temp Calc of sum of memory needed for the next row
+		// - x11: 	Temp actual row
+		// - x12: 	Temp actual column
 
 	//Saved values used:
 		// - x20:	Framebuffer Base Address
@@ -217,17 +67,17 @@ createRectangle:
 	sub x10, x10, x6
 	lsl x10, x10, 2
 
-	mov x3, x5 //Set Rectangle Height
+	mov x12, x5 //Set Rectangle Height
 	resetRectW:
-	mov x4, x6 //Set and Reset Rectangle Width
+	mov x11, x6 //Set and Reset Rectangle Width
 	nxtPixelRect:
 		stur x0,[x9]	   	 	// Set color of pixel N
 		add x9,x9,4	   			// Next pixel
-		sub x4,x4,1	   			// decrement X counter
-		cbnz x4,nxtPixelRect   	// If not end row jump
+		sub x11,x11,1	   			// decrement X counter
+		cbnz x11,nxtPixelRect   	// If not end row jump
 		add x9, x9, x10	 		// Direccion actual + restante para la sig fila = x19 + (SCREEN_WIDTH - FLOOR_WIDTH)*4
-		sub x3,x3,1	    	 	// Decrement Y counter
-		cbnz x3,resetRectW 		// if not last row, jump
+		sub x12,x12,1	    	 	// Decrement Y counter
+		cbnz x12,resetRectW 		// if not last row, jump
 	
 	//Carga del registro de return y devolucion del siguiente
 	ldr x30, [sp]
@@ -239,22 +89,32 @@ createTriangle:
 	sub sp, sp, 16
 	str x30, [sp]
 
-		// Input values:
-	// - x0: Color of Triangle
-	// - x1: Coord del primero en Y
-	// - x2: Coord del primero en X
-	// - x5: Triangle Height
-	// - x6: Triangle Width
+	// Input values:
+		// - x0: Color of Triangle
+		// - x1: Coord del primero en Y
+		// - x2: Coord del primero en X
+		// - x3: Alto Escalon
+		// - x4: Ancho Escalon
+		// - x5: Ancho del Triangulo
+		// - x6: Cantidad de escalones
+
+	//Temporary values:
+		// - x9: 	Temp Alto Escalon
+		// - x10:	Temp Ancho Escalon
+
+	mov x9, x3
+	mov x3, 0x00000000
+	mov x10, x4
+	mov x4, 0x00000000
 
 	loop_Triang2:
 		bl createRectangle
-	loop_Triang :
-		sub x1, x1, 3  //Disminuyo el tamaño X
-		add x2, x2, 3  //Aumento la Tamaño Y
-		sub x6, x6, 6	 //Va disminuyendo el ancho de cada escalon
-		cmp x6, XZR		// Comparo x6 con 0
-		b.GT loop_Triang2 //Me aseguro que el resultado no sea negativo.
-
+		loop_Triang :
+			sub x1, x1, 3  //Disminuyo el tamaño X
+			add x2, x2, 3  //Aumento la Tamaño Y
+			sub x6, x6, 6	 //Va disminuyendo el ancho de cada escalon
+			cmp x6, XZR		// Comparo x6 con 0
+			b.GT loop_Triang2 //Me aseguro que el resultado no sea negativo.
 
 	//Carga del registro de return y devolucion del siguiente
 	ldr x30, [sp]
@@ -268,6 +128,7 @@ delay:
 
     // Input values:
         // - x7: Limit to repeat
+
     cont:
         sub x7, x7, 1
         cbnz x7, cont
@@ -275,11 +136,6 @@ delay:
     ldr x30, [sp]
 	add sp, sp, 16
     ret
-
-//---------------------------------------------------------------
-// Infinite Loop 
-InfLoop: 
-	b InfLoop
 
 exitProgram:
 	.end
